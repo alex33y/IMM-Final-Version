11916cef1b24f99f0568ff217711ef7c45575d67
alex33y <152503680+alex33y@users.noreply.github.com>
2024-12-11 22:58:27 +0000

    Update OpenURL.cs

--- Assets/Scripts/OpenURL.cs
+++ Assets/Scripts/OpenURL.cs
@@ -5,6 +5,6 @@ using UnityEngine;
 public class OpenURL : MonoBehaviour
 {
     public void OpenUrl(){
-        Application.OpenURL("https://github.com/alex33y/IMM-Final-Version/settings");
+        Application.OpenURL("https://github.com/alex33y/IMM-Final-Version");
     }
 }
